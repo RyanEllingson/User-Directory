37c7b0cfbb492d9fd1d72dc9135eaafa2d858d3d
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-01-28 13:42:27 -0600

    sort function works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import userlist from "./users.json";
+import Userform from "./components/Userform";
 
 function App() {
+  const [users, setUsers] = useState(userlist);
+  
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
+
+  const swap = function(items, firstIndex, secondIndex) {
+    let temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+    return items;
+  };
+
+  const sortUsers = function(userArray) {
+    console.log("button clicked!");
+    let tempArray = [...userArray];
+    for (let i=0; i<tempArray.length; i++) {
+      for (let j=i; j<tempArray.length; j++) {
+        if (tempArray[j].name < tempArray[i].name) {
+          console.log("swapping element " + i + " and element " + j);
+          tempArray = swap(tempArray, i, j);
+        }
+      }
+    }
+    console.log(tempArray);
+    setUsers(tempArray);
+  };
+
+  const userList = users.map((userObj) => 
+    <Userform handleClick={()=>{setUsers(users.filter(userEl => userEl.id === userObj.id))}} name={userObj.name} />
+  );
+
+  const sortButton = <button type="button" className="btn btn-secondary" onClick={() => {sortUsers(users)}}>Sort users alphabetically</button>;
+
+  console.log("@#*$^@#*$&^@#*^$@#*&$^", users)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            {userList}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            {sortButton}
+          </div>
+          <div className="col-6">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
