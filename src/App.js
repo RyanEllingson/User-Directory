a45c5620960cce854337ee1c24802faab421eea9
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-01-28 20:01:05 -0600

    user list generated using Axios http request; can filter users by last name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import userlist from "./users.json";
+// import userlist from "./users.json";
 import Userform from "./components/Userform";
+import Axios from "axios";
 
 function App() {
-  const [users, setUsers] = useState(userlist);
+  let userlist = [];
+
+  let initList = [];
   
+  const [users, setUsers] = useState(userlist);
+  const [initUsers, setInitUsers] = useState(initList);
+  const [search, setSearch] = useState("");
+
+  const userRequest = async function() {
+    const queryUrl = "https://randomuser.me/api/?results=20";
+    const response = await Axios.get(queryUrl);
+    userlist = response.data.results.map((result) => {
+      return {
+        firstName: result.name.first,
+        lastName: result.name.last
+      }
+    });
+    setUsers(userlist);
+    setInitUsers(userlist);
+  }
+
   useEffect(() => {
-    console.log(users);
-  }, [users]);
+    userRequest();  
+  }, []);
 
   const swap = function(items, firstIndex, secondIndex) {
     let temp = items[firstIndex];
@@ -22,42 +42,55 @@ function App() {
     let tempArray = [...userArray];
     for (let i=0; i<tempArray.length; i++) {
       for (let j=i; j<tempArray.length; j++) {
-        if (tempArray[j].name < tempArray[i].name) {
+        if (tempArray[j].lastName < tempArray[i].lastName) {
           console.log("swapping element " + i + " and element " + j);
           tempArray = swap(tempArray, i, j);
         }
       }
     }
-    console.log(tempArray);
     setUsers(tempArray);
+    console.log(userlist);
   };
 
+  const searchUsers = function() {
+    console.log(search);
+    setUsers(users.filter(userEl => userEl.lastName.includes(search)));
+  }
+  
+
+
   const userList = users.map((userObj) => 
-    <Userform handleClick={()=>{setUsers(users.filter(userEl => userEl.id === userObj.id))}} name={userObj.name} />
+    <Userform name={userObj.firstName + " " + userObj.lastName} />
   );
 
-  const sortButton = <button type="button" className="btn btn-secondary" onClick={() => {sortUsers(users)}}>Sort users alphabetically</button>;
+  const sortButton = <button type="button" className="btn btn-secondary mb-3" onClick={() => {sortUsers(users)}}>Sort users alphabetically</button>;
 
-  console.log("@#*$^@#*$&^@#*^$@#*&$^", users)
+  const resetButton = <button type="button" className="btn btn-success mb-3" onClick={() => setUsers([...initUsers])}>Restore original users</button>;
+
   return (
     <div className="App">
-      <div className="container">
+      <h1 className="text-center">User Directory</h1>
+      <div className="container my-3">
         <div className="row">
-          <div className="col-12">
+          <div className="col-3">
+            {sortButton}
+            {resetButton}
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" value={search} onChange={({target}) => setSearch(target.value)}/>
+              <div className="input-group-append">
+                <button onClick={searchUsers} className="btn btn-outline-secondary" type="button" id="button-addon2">Search by last name</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
             {userList}
           </div>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            {sortButton}
-          </div>
-          <div className="col-6">
-
-          </div>
+          <div className="col-3"/>
         </div>
       </div>
     </div>
   );
+
 }
 
 export default App;
--- src/components/Userform/index.js
+++ src/components/Userform/index.js
@@ -4,9 +4,6 @@ import "./style.css";
 function Userform(props) {
   return (
     <div className="input-group mb-3">
-        <div className="input-group-prepend">
-            <button className="btn btn-outline-secondary" onClick = {props.handleClick} type="button" id="button-addon1">Filter by this user</button>
-        </div>
         <input type="text" className="form-control-plaintext" readOnly placeholder={"  " + props.name} aria-label="Example text with button addon" aria-describedby="button-addon1"/>
     </div>
   );
